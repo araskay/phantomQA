da4f6a4701ab40dafcfffa3632c397301526f0a3
Aras Kayvanrad <mkayvan@gmail.com>
2019-04-05 13:40:58 -0400

    update run_acf_analysis_spikecor_slc_detrend_fit.R calling acf_analysis_MCDout()

--- run_acf_analysis_spikecor_slc_detrend_fit.R
+++ run_acf_analysis_spikecor_slc_detrend_fit.R
@@ -2,9 +2,11 @@
 # library(rmarkdown)
 # render('run_acf_analysis_spikecor_slc_detrend_fit.R')
 
-acfdir <- "~/Dropbox/analysis/phantomQA/acf/spikecor_slc_detrend_fit"
+workingdir <- "~/Dropbox/analysis/phantomQA/acf/spikecor_slc_detrend_fit"
 
-setwd(acfdir)
+setwd(workingdir)
+
+acf_file <- 'acfFWHM.csv'
 
 spotfire_file <- '../../fBIRN_MR_QA_20180820.csv'
 fbirnQA_file <- 'subjects_spikecor_slc_fbirnQA.csv'
@@ -13,7 +15,7 @@ acf_rem_suffix <- '_spikecor_3dDetrend'
 fbirn_rem_suffix <- '_spikecor'
 
 source("~/Dropbox/code/phantomQA/get_data.R")
-data <- get_data(acfdir = acfdir, fbirnQA_file = fbirnQA_file, spotfire_file = spotfire_file,
+data <- get_data(acfFWHM_file = acf_file, fbirnQA_file = fbirnQA_file, spotfire_file = spotfire_file,
                  acf_rem_suffix = acf_rem_suffix, fbirn_rem_suffix = fbirn_rem_suffix)
 
 plot_title <- 'spikecor_slc_detrend_fit'
@@ -28,7 +30,7 @@ print_physical_anomalies(data = data, sites = sites)
 
 
 source('~/Dropbox/code/phantomQA/acf_analysis_MCDout.R')
-res <- acf_analysis_MCDout(data = data, output_dir = acfdir,
+res <- acf_analysis_MCDout(data = data,
                            plot_title = plot_title,
                            show.pca.varaxes = T,
                            show.pca.timecourse = T,
