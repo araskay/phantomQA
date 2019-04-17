046dcfe568cf2a39c84d7211094d27597be1f1e2
Aras Kayvanrad <mkayvan@gmail.com>
2019-04-17 17:37:54 -0400

    MCD outlier replaced with physical anomaly

--- run_acf_analysis_spikecor_slc_detrend_fit.R
+++ run_acf_analysis_spikecor_slc_detrend_fit.R
@@ -2,6 +2,9 @@
 # library(rmarkdown)
 # render('run_acf_analysis_spikecor_slc_detrend_fit.R')
 
+source("~/Dropbox/code/phantomQA/get_data.R")
+source('~/Dropbox/code/phantomQA/acf_analysis.R')
+
 workingdir <- "~/Dropbox/analysis/phantomQA/acf/spikecor_slc_detrend_fit"
 
 setwd(workingdir)
@@ -14,7 +17,6 @@ fbirnQA_file <- 'subjects_spikecor_slc_fbirnQA.csv'
 acf_rem_suffix <- '_spikecor_3dDetrend'
 fbirn_rem_suffix <- '_spikecor'
 
-source("~/Dropbox/code/phantomQA/get_data.R")
 data <- get_data(acfFWHM_file = acf_file, fbirnQA_file = fbirnQA_file, spotfire_file = spotfire_file,
                  acf_rem_suffix = acf_rem_suffix, fbirn_rem_suffix = fbirn_rem_suffix)
 
@@ -24,20 +26,14 @@ plot_title <- 'spikecor_slc_detrend_fit'
 
 # detect anomalies based on physical quantities of fwhm
 sites <- c("BYC", "CAM", "MCM", "QNS", "SBH", "TBR", "SMH", "TOH", "TWH", "UBC", "UCA", "UTO", "WEU")
-source('~/Dropbox/code/phantomQA/acf_analysis_physical_anomalies.R')
 
-#res <- physical_anomalies(data = data,sites = sites)
-print_physical_anomalies(data = data, sites = sites)
-
-
-
-source('~/Dropbox/code/phantomQA/acf_analysis_MCDout.R')
-res <- acf_analysis_MCDout(data = data,
-                           plot_title = plot_title,
-                           show.pca.varaxes = T,
-                           show.pca.timecourse = T,
-                           save_png = T,
-                           pca.var_axes_separate = T)
+res <- acf_analysis(data = data,
+                    plot_title = plot_title,
+                    show.pca.varaxes = T,
+                    show.pca.timecourse = T,
+                    save_png = T,
+                    pca.var_axes_separate = F,
+                    anom_coef = 10)
 
 nrow(res$data_out)
 print(res$out_sessions)
