a8fb76ee9318db553b8b9e33a9fe80fe70ed9803
Aras Kayvanrad <mkayvan@gmail.com>
2019-04-22 18:16:13 -0400

    modified additional_plots.R - combined total and per-volume anomalies plot

--- additional_plots.R
+++ additional_plots.R
@@ -17,17 +17,22 @@ data_spikecor <- read.csv(paste(spikecor_dir,'/data_clean.csv',sep = ''))
 ## ACF FWHM bar plots
 source('~/Dropbox/code/phantomQA/plotting.R')
 p_unproc <- get_fwhm_sub_barplots(data_unproc)
-#p_spikecor <- get_fwhm_sub_barplots(data_spikecor)
+p_spikecor <- get_fwhm_sub_barplots(data_spikecor)
 
 p_unproc$p_maxFWHMx <- p_unproc$p_maxFWHMx + theme(legend.title=element_blank())
 mylegend <- g_legend(p_unproc$p_maxFWHMx)
-png(filename='detrend_fit_acf_min_max_fwhm.png', width = 21, height = 10, units = "cm", res = 600)
+png(filename='acf_min_max_fwhm.png', width = 21, height = 10, units = "cm", res = 600)
 grid.arrange(arrangeGrob(arrangeGrob(p_unproc$p_maxFWHMx + theme(legend.position="none"),
                                      p_unproc$p_maxFWHMy + theme(legend.position="none"),
                                      p_unproc$p_minFWHMx + theme(legend.position="none"),
                                      p_unproc$p_minFWHMy + theme(legend.position="none"),
-                                     ncol = 2, nrow = 2, top = 'Min vs. Max FWHM'),
-                         ncol = 1, nrow = 1),
+                                     ncol = 2, nrow = 2, top = '(a) Unprocessed'),
+                         arrangeGrob(p_spikecor$p_maxFWHMx + theme(legend.position="none"),
+                                     p_spikecor$p_maxFWHMy + theme(legend.position="none"),
+                                     p_spikecor$p_minFWHMx + theme(legend.position="none"),
+                                     p_spikecor$p_minFWHMy + theme(legend.position="none"),
+                                     ncol = 2, nrow = 2, top = '(b) SpikeCor processed'),
+                         ncol = 1, nrow = 2),
              mylegend,
              ncol=2, widths=c(5,1))
 dev.off()
@@ -40,26 +45,17 @@ p_spikecor <- get_fwhm_sub_barplots(data_spikecor)
 
 p_unproc$p_maxFWHMx <- p_unproc$p_maxFWHMx + theme(legend.title=element_blank())
 mylegend <- g_legend(p_unproc$p_maxFWHMx)
-png(filename='anomaly_per_vol.png', width = 21, height = 24, units = "cm", res = 600)
+png(filename='anomaly_per_vol.png', width = 21, height = 8, units = "cm", res = 600)
 grid.arrange(arrangeGrob(arrangeGrob(p_unproc$p_medAnomalyPerVolx + theme(legend.position="none"),
                                      p_unproc$p_medAnomalyPerVoly + theme(legend.position="none"),
-                                     p_unproc$p_q1AnomalyPerVolx + theme(legend.position="none"),
-                                     p_unproc$p_q1AnomalyPerVoly + theme(legend.position="none"),
-                                     p_unproc$p_q3AnomalyPerVolx + theme(legend.position="none"),
-                                     p_unproc$p_q3AnomalyPerVoly + theme(legend.position="none"),
-                                     ncol = 2, nrow = 3, top = '(a) Unprocessed'),
+                                     ncol = 2, nrow = 1, top = '(a) Unprocessed'),
                          arrangeGrob(p_spikecor$p_medAnomalyPerVolx + theme(legend.position="none"),
                                      p_spikecor$p_medAnomalyPerVoly + theme(legend.position="none"),
-                                     p_spikecor$p_q1AnomalyPerVolx + theme(legend.position="none"),
-                                     p_spikecor$p_q1AnomalyPerVoly + theme(legend.position="none"),
-                                     p_spikecor$p_q3AnomalyPerVolx + theme(legend.position="none"),
-                                     p_spikecor$p_q3AnomalyPerVoly + theme(legend.position="none"),
-                                     ncol = 2, nrow = 3, top = '(b) SpikeCor processed'),
+                                     ncol = 2, nrow = 1, top = '(b) SpikeCor processed'),
                          ncol = 1, nrow = 2),
              mylegend,
              ncol=2, widths=c(5,1))
 dev.off()
-
 
 ## total number of anomalies bar plots
 source('~/Dropbox/code/phantomQA/plotting.R')
@@ -80,3 +76,24 @@ grid.arrange(arrangeGrob(arrangeGrob(p_unproc$p_numAnomaly_x + theme(legend.posi
              ncol=2, widths=c(5,1))
 dev.off()
 
+## total number of anomalies and (# anomalies)/vol in a single plot
+png(filename='anomalies.png', width = 21, height = 16, units = "cm", res = 600)
+grid.arrange(arrangeGrob(arrangeGrob(rbind(ggplotGrob(p_unproc$p_numAnomaly_x + theme(legend.position="none")),
+                                           ggplotGrob(p_unproc$p_medAnomalyPerVolx + theme(legend.position="none")),
+                                           size = "first"),
+                                     rbind(ggplotGrob(p_unproc$p_numAnomaly_y + theme(legend.position="none")),
+                                           ggplotGrob(p_unproc$p_medAnomalyPerVoly + theme(legend.position="none")),
+                                           size = "first"),
+                                     ncol=2, top = '(a) Unprocessed'),
+                         arrangeGrob(rbind(ggplotGrob(p_spikecor$p_numAnomaly_x + theme(legend.position="none")),
+                                           ggplotGrob(p_spikecor$p_medAnomalyPerVolx + theme(legend.position="none")),
+                                           size = "first"),
+                                     rbind(ggplotGrob(p_spikecor$p_numAnomaly_y + theme(legend.position="none")),
+                                           ggplotGrob(p_spikecor$p_medAnomalyPerVoly + theme(legend.position="none")),
+                                           size = "first"),
+                                     ncol=2, top = '(a) SpikeCor processed'),
+                         
+                         nrow = 2),
+             mylegend,
+             ncol=2, widths=c(5,1))
+dev.off()
