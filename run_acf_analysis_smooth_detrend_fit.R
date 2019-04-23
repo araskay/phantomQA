8614bb8eea19f8893f2b2e5385a12882b4cea185
Aras Kayvanrad <mkayvan@gmail.com>
2019-04-23 16:53:41 -0400

    updated run_acf_analysis_smooth_detrend_fit.R to use new functions for physical anomaly instead of old MCD

--- run_acf_analysis_smooth_detrend_fit.R
+++ run_acf_analysis_smooth_detrend_fit.R
@@ -2,51 +2,37 @@
 # library(rmarkdown)
 # render('run_acf_analysis_smooth_detrend_fit.R')
 
-acfdir <- "~/Dropbox/analysis/phantomQA/acf/smooth_detrend_fit"
+source("~/Dropbox/code/phantomQA/get_data.R")
+source('~/Dropbox/code/phantomQA/acf_analysis.R')
 
-setwd(acfdir)
+working_dir <- "~/Dropbox/analysis/phantomQA/acf/smooth_detrend_fit"
+
+setwd(working_dir)
+
+acf_file <- 'acfFWHM.csv'
 
 spotfire_file <- '../../fBIRN_MR_QA_20180820.csv'
 fbirnQA_file <- 'subjects_smooth_fbirnQA.csv'
 
-acf_rem_suffix <- '_3dBlurToFWHM_3dDetrend.nii.gz'
+acf_rem_suffix <- '_3dBlurToFWHM_3dDetrend'
 fbirn_rem_suffix <- '_3dBlurToFWHM'
 
-source("~/Dropbox/code/phantomQA/get_data.R")
-data <- get_data(acfdir = acfdir, fbirnQA_file = fbirnQA_file, spotfire_file = spotfire_file,
+data <- get_data(acfFWHM_file = acf_file, fbirnQA_file = fbirnQA_file, spotfire_file = spotfire_file,
                  acf_rem_suffix = acf_rem_suffix, fbirn_rem_suffix = fbirn_rem_suffix)
 
-nrow(data)
+write.csv(data, file = "data_clean.csv")
 
 plot_title <- 'smooth_detrend_fit'
+sites <- c("BYC", "CAM", "MCM", "QNS", "SBH", "TBR", "SMH", "TOH", "TWH", "UBC", "UCA", "UTO", "WEU")
 
-source('~/Dropbox/code/phantomQA/acf_analysis_MCDout.R')
-res_smooth <- acf_analysis_MCDout(data = data, output_dir = acfdir, plot_title = plot_title,
-                                  show.pca.varaxes = T,
-                                  show.pca.timecourse = F,
-                                  save_png = T,
-                                  pca.var_axes_separate = T)
+res_smooth <- acf_analysis(data = data,
+                    plot_title = plot_title,
+                    show.pca.varaxes = T,
+                    show.pca.timecourse = T,
+                    save_png = T,
+                    pca.var_axes_separate = F,
+                    anom_coef = 10)
 
 nrow(res_smooth$data_out)
 cat(paste(res_smooth$out_sessions$BYC, collapse = '\n'))
-
-####################################################
-# compare outlier detection based on acf and fbirn
-####################################################
-vars_fbirn <- c("mean","SFNR","std","percentFluc","drift","driftfit","rdc",
-                "minFWHMX","minFWHMY","minFWHMZ","maxFWHMX",
-                "meanGhost","meanBrightGhost","SNR")
-
-
-vars_acf <- c("minFWHMx","maxFWHMx","medFWHMx","q1FWHMx","q3FWHMx","meanFWHMx","stdFWHMx",
-              "minFWHMy","maxFWHMy","medFWHMy","q1FWHMy","q3FWHMy","meanFWHMy","stdFWHMy")
-
-sites <- c("BYC", "CAM", "MCM", "QNS", "SBH", "TBR", "SMH", "TOH", "TWH", "UBC", "UCA", "UTO", "WEU")
-#res_smooth_acf <- remove_outlier_session(data = data, sites = sites, vars = vars_acf)
-
-# the following throws an error for SMH
-#res_smooth_fbirn <- remove_outlier_session(data = data, sites = sites, vars = vars_fbirn)
-
-#nrow(data)
-#nrow(res_smooth_acf$data_out)
 
